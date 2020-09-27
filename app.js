3d664120df5832acf18352185fb06a6da1937e8e
vysakh v <vysakhv1998@github.com>
2020-09-28 01:04:40 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,18 +1,192 @@
-var ctx = document.getElementById('myChart');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+var chart = new CanvasJS.Chart("chartContainer1", {
 
 
+    animationEnabled: true,
+
+    data: [{
+        type: "doughnut",
+        innerRadius: "70%",
+
+        indexLabel: "{label}: #percent%",
+
+        dataPoints: [{
+                label: "New User",
+                y: 99616
+            },
+            {
+                label: "Unknown User",
+                y: 1000
+            },
+            {
+                label: "Known User",
+                y: 99530
+            }
+
+
+        ]
+    }, ]
 });
+chart.render();
+
+var chart = new CanvasJS.Chart("chartContainer2", {
+
+
+    animationEnabled: true,
+
+    data: [{
+        type: "doughnut",
+        innerRadius: "70%",
+
+        indexLabel: "{label}: #percent%",
+
+        dataPoints: [{
+                label: "Web",
+                y: 50
+            },
+            {
+                label: "Mobile",
+                y: 85
+            },
+
+
+
+        ]
+    }, ]
+});
+chart.render();
+
+var chart = new CanvasJS.Chart("chartContainer3", {
+
+
+    animationEnabled: true,
+
+    data: [{
+        type: "doughnut",
+        innerRadius: "70%",
+
+        indexLabel: "{label}: #percent%",
+
+        dataPoints: [{
+                label: "App",
+                y: 356
+            },
+            {
+                label: "Email",
+                y: 3000
+            },
+            {
+                label: "SMS",
+                y: 8907
+            },
+            {
+                label: "Web Push",
+                y: 50
+            }
+
+
+        ]
+    }, ]
+});
+chart.render();
+
+
+
+
+var chart = new CanvasJS.Chart("chartContainer4", {
+
+
+    animationEnabled: true,
+
+    data: [{
+
+        type: "column",
+
+        dataPoints: [{
+                label: "App Push",
+                y: 0
+            },
+            {
+                label: "SMS",
+                y: 15
+            },
+            {
+                label: "Web Push",
+                y: 25
+            },
+            {
+                label: "Email",
+                y: 70
+            },
+            {
+                label: "Web Pop-Up",
+                y: 28
+            }
+        ]
+    }, ]
+});
+chart.render();
+
+
+var chart = new CanvasJS.Chart("chartContainer5", {
+
+
+    animationEnabled: true,
+
+    data: [{
+        type: "pie",
+        innerRadius: "70%",
+
+        indexLabel: "{label}: #percent%",
+
+        dataPoints: [{
+                label: "Users",
+                y: 10
+            },
+            {
+                label: "Campaign",
+                y: 40
+            },
+            {
+                label: "Conversion",
+                y: 20
+            }
+
+
+        ]
+    }, ]
+});
+chart.render();
+
+var chart = new CanvasJS.Chart("chartContainer6", {
+
+
+    animationEnabled: true,
+
+    data: [{
+        type: "pie",
+        innerRadius: "70%",
+
+        indexLabel: "{label}: #percent%",
+
+        dataPoints: [{
+                label: "Users",
+                y: 10
+            },
+            {
+                label: "Campaign",
+                y: 40
+            },
+            {
+                label: "Conversion",
+                y: 20
+            },
+            {
+                label: "Revenue",
+                y: 20
+            }
+
+
+        ]
+    }, ]
+});
+chart.render();
